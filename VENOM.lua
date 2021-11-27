2b564664b2ce1dce043f01c7afa5ae64e33a56af
ahmedfrawn235 <94080598+ahmedfrawn235@users.noreply.github.com>
2021-11-27 17:29:43 +0200

    Add files via upload

--- VENOM.lua
+++ VENOM.lua
@@ -3890,7 +3890,7 @@ local bio = json.result.bio
 keyboard = {} 
 keyboard.inline_keyboard = {{{text = name,url="t.me/"..user}},}
 local msg_id = msg.id_/2097152/0.5
-https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/ ..user..'&caption=' .. URL.escape(bio).."&reply_to_message_id="..msg_id.."&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
+https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/'..user..'&caption=' .. URL.escape(bio).."&reply_to_message_id="..msg_id.."&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
 end
 if SourceCh(msg) and text =='فرعون'or text =='داد'or text =='1'then
 local Text = [[
